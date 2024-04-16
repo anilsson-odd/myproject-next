28712fc3805dc5eb29581f16362c7eb56b2721b4
Andreas Nilsson <ani@fsfe.org>
2024-04-16 10:42:58 +0200

    Fixed dynamic route for the categories to display a page

--- src/app/store/products/[category]/page.js
+++ src/app/store/products/[category]/page.js
@@ -1,5 +1,18 @@
 const CategoryPage = ({ params }) => {
-  return <div>{params.category}</div>;
+  return (
+    <div className='bg-slate-50 h-screen'>
+      <div className='flex'>
+        <div className='w-3/4'>
+          <h1 className='text-4xl font-bold text-center mt-4'>{params.category.charAt(0).toUpperCase() + params.category.slice(1)}</h1>
+        </div>
+        <div className='w-1/4'>
+          <div className='mt-16'>
+            List
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CategoryPage;
