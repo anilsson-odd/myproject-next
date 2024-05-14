901cdf8df14f4a40d46851b130c17e707467ea9e
Andreas Nilsson <ani@fsfe.org>
2024-05-14 10:22:12 +0200

    Fixed Signup Page design

--- src/app/store/register/page.js
+++ src/app/store/register/page.js
@@ -1,6 +1,10 @@
 "use client";
 
+import { Button, Card, CardBody, Input } from "@nextui-org/react";
+import { useState } from "react";
+
 const SignupPage = () => {
+  const [message, setMessage] = useState("");
   async function handleSignUp(e) {
     e.preventDefault();
 
@@ -9,42 +13,49 @@ const SignupPage = () => {
     const name = formData.get("name");
     const password = formData.get("password");
 
-    const result = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/auth/sign-up`, {
-      method: "POST",
-      body: JSON.stringify({
-        email,
-        name,
-        password,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+    try {
+      const result = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/auth/sign-up`, {
+        method: "POST",
+        body: JSON.stringify({
+          email,
+          name,
+          password,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
 
-    const data = await result.json();
+      const data = await result.json();
+
+      setMessage(data.message);
+    } catch (err) {
+      console.log(err.message);
+    }
   }
 
   return (
-    <>
-      <h1>Register</h1>
-      <form
-        onSubmit={handleSignUp}
-      >
-        <label>
-          Email
-          <input name='email' type='email' />
-        </label>
-        <label>
-          Name
-          <input name="name" type="text" />
-        </label>
-        <label>
-          Password
-          <input name='password' type='password' />
-        </label>
-        <button type='submit'>Sign Up</button>
+    <div className="flex flex-col items-center">
+    <form
+      onSubmit={handleSignUp}
+    >
+      <h1 className="text-xl mt-5 text-center">Register</h1>
+        <Card>
+          <CardBody>
+        <div className="flex flex-col justify-center items-center">
+          <Input className="w-96" name='email' type='email' label="Email" placeholder="Enter your email" />
+          <br />
+          <Input className="w-96" name="name" type="text" label="Name" placeholder="Enter your full name" />
+          <br />
+          <Input className="w-96" name='password' type='password' label="Password" placeholder="Enter your password" />
+          <br />
+          {message}
+          <Button type='submit'>Sign Up</Button>
+        </div>
+        </CardBody>
+        </Card>
       </form>
-    </>
+    </div>
   );
 }
 
